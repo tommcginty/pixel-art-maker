b50cc22ba7893a89e9635bbd707dc4b8117dea7c
Tom McGinty <thmcginty@gmail.com>
2018-12-29 12:31:22 -0500

    Commits entire project

--- designs.js
+++ designs.js
@@ -1,29 +1,49 @@
 const pixelCanvas = document.getElementById('pixelCanvas');
-const sizePicker = $('#sizePicker');
+//const sizePicker = $('#sizePicker');
+
+function clearGrid(){
+  let x = document.getElementById('pixelCanvas').rows.length;
+  for (let i = 0; i < x; i++)
+    pixelCanvas.removeChild(pixelCanvas.childNodes[0]);
+};
 
 function makeGrid(){
-  // Clear previous grid
-  $('tbody').remove();
   // Select size input
-  let m = $('#inputHeight').val();
-  let n = $('#inputWidth').val();
+  let m = document.getElementById('inputHeight').value;
+  let n = document.getElementById('inputWidth').value;
   // Create grid
-  for (let i = 0; i < m; i++)
-    pixelCanvas.append('<tr></tr>');
-  for (let j = 0; j < n; j++)
-    $('tr').append('<td></td>');
+  for (let i = 0; i < m; i++){
+    let tableRow =  document.createElement('TR');
+    tableRow.setAttribute('id', 'row' + i);
+    pixelCanvas.append(tableRow);
+    for (let j = 0; j < n; j++){
+      let tableColumn = document.createElement('TD');
+      tableColumn.setAttribute('class', 'box');
+      let gridBox = document.getElementById('row'+i);
+      gridBox.append(tableColumn);
+      }
+    }
   };
 
+function changeColor(evt) {
+    console.log('I was clicked: ' + evt.target);
+    let newColor = document.getElementById('colorPicker').value;
+    evt.target.style.backgroundColor = newColor;
+};
+
  // When size is submitted by the user, call makeGrid()
-sizePicker.submit(function(event) {
+function submitSize(){
   event.preventDefault();
+  clearGrid();
   makeGrid();
-  });
+  };
+document.getElementById('sizePicker').onsubmit = submitSize;
+pixelCanvas.addEventListener('click', changeColor);
 
 // When a square is clicked,
 // change the color to the color selected in the color picker
+/*
 pixelCanvas.click(function(event) {
   // Select color input
-   let pixelColor = $('#colorPicker').val();
-  $(event.target).closest('td').css('background-color', pixelColor);
   });
+*/
